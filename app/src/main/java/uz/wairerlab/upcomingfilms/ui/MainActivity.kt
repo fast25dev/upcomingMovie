8d03bdfd4a264945d8997a6567c7b213a8c4cbf9
bogibekdev <bogibekdev@gmail.com>
2023-09-17 07:17:50 +0500

    initial commit

---
+++ app/src/main/java/uz/wairerlab/upcomingfilms/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package uz.wairerlab.upcomingfilms.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import uz.wairerlab.upcomingfilms.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/uz/wairerlab/upcomingfilms/util/Extension.kt
@@ -0,0 +1,7 @@
+package uz.wairerlab.upcomingfilms.util
+
+object Extensions {
+
+    fun String.imageUrl()= "https://image.tmdb.org/t/p/original$this"
+
+}
